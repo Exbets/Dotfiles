a37a15bd499211b34c976ff19c57ea929f395bf9
exbets <domgorkoff@gmail.com>
2026-02-27 13:12:27 -0800

    Revert "moved stuff around in home.nix and added comments"
    This reverts commit 2ee9ab52c9b11af43545691f3b389ac13867bb92.

--- nixos/AM5/home.nix
+++ nixos/AM5/home.nix
@@ -69,9 +69,8 @@
       postman # API Client
       distroshelf # GUI for Distrobox Containers
       gdu # Disk Usage Analyzer
-      spotify # Spotify Music
-      protonvpn-gui # VPN
-      lutris # Game Launcher
+      spotify
+      protonvpn-gui
     ];
 
     utils = [
@@ -85,6 +84,7 @@
       pywal # Generates Colour Themes
       cmus # CLI music player
       distrobox # Wrapper around podman or docker to create and start containers
+      lutris
       unzip
       unar
     ];
